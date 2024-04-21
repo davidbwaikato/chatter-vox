a4d9be056b075dd38464dd9d687366c0b951c172
David Bainbridge <davidb@waikato.ac.nz>
2024-04-22 10:33:16 +1200

    Better filename for this

--- src/app/components/AudioSpectrumVisualizer/MediaPlayer.ts
+++ src/app/components/AudioSpectrumVisualizer/MediaPlayer.ts
@@ -1,4 +1,8 @@
 
+// Inspired by MediaRecorder, MediaPlayer is a class that
+// is used to to keep state of what is happening in terms
+// of the playing audio
+
 class MediaPlayer {
   public state: string;
   
