497b8eebb453ed96676d268b48097bbe17a12add
David Bainbridge <davidb@waikato.ac.nz>
2024-04-26 11:46:10 +1200

    Icons for audio-player

---
+++ src/app/components/IconRewind.jsx
@@ -0,0 +1,27 @@
+
+const IconRewind = (props) => {
+
+    return (
+	    <div className="audioplayer-icon-svg">
+	      <div className="audioplayer-icon-svg-container">
+                <svg xmlns="http://www.w3.org/2000/svg"
+                     id="rewind"
+                     viewBox="2 2 76 76">
+                  <g>
+                    <path
+                      d="M39.75,5.5C20.834,5.5,5.5,20.834,5.5,39.75C5.5,58.666,20.834,74,39.75,74C58.666,74,74,58.666,74,39.75   C74,20.834,58.666,5.5,39.75,5.5z M39.75,70C23.043,70,9.5,56.457,9.5,39.75C9.5,23.043,23.043,9.5,39.75,9.5   C56.457,9.5,70,23.043,70,39.75C70,56.457,56.457,70,39.75,70z" />
+                    <g transform="translate(2.7884615,0.09615385)">
+                      <path
+                        d="m 29.265203,40.480769 25.2,-14.4 v 28.8 z"
+                        style={{fill:'#000000',stroke:'#000000',strokeWidth:3.2,strokeLinejoin:'round',strokeMiterlimit:4,strokeDasharray:'none',strokeOpacity:1}} />
+                      <path
+                        d="m 26.75,52.389423 c 0,2.348 -1.903,4.25 -4.25,4.25 v 0 c -2.347,0 -4.25,-1.902 -4.25,-4.25 v -23.625 c 0,-2.347 1.903,-4.25 4.25,-4.25 v 0 c 2.347,0 4.25,1.903 4.25,4.25 z" />
+                    </g>
+                  </g>
+                </svg>                
+	      </div>
+            </div>
+  );
+};
+
+export { IconRewind };
